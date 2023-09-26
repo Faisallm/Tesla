97bb3980904737d7e899f46b991c81599d782fab
Faisallm <faisallawan1997@gmail.com>
2023-09-26 10:06:05 -0700

    gear 1st

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,66 @@ import React from "react";
 import styled from "styled-components";
 import Section from "./Section";
 
+import modelS from "../assets/images/model-s.jpg";
+import modelY from "../assets/images/model-y.jpg";
+import model3 from "../assets/images/model-3.jpg";
+import modelX from '../assets/images/model-x.jpg';
+import solarPanel from '../assets/images/solar-panel.jpg';
+import solarRoof from '../assets/images/solar-roof.jpg';
+import Accessories from '../assets/images/accessories.jpg'; 
+
+
 const Home = () => {
   return (
     <Container>
-      <Section />
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImage={modelS}
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        backgroundImage={modelY}
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        backgroundImage={model3}
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        backgroundImage={modelX}
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Lowest Cost Solar Panels in Nigeria"
+        description="Money-back guarantee"
+        backgroundImage={solarPanel}
+        leftBtnText="Order Now"
+        rightBtnText="Learn more"
+      />
+      <Section
+        title="Solar for New Roofs"
+        description="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
+        backgroundImage={solarRoof}
+        leftBtnText="Order Now"
+        rightBtnText="Learn more"
+      />
+      <Section
+        title="Accessories"
+        description=""
+        backgroundImage={Accessories}
+        leftBtnText="Shop now"
+      />
     </Container>
   );
 };
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,18 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 import modelS from "../assets/images/model-s.jpg";
+import downArrow from "../assets/images/down-arrow.svg";
 
-const Section = () => {
+const Section = ({
+  title,
+  description,
+  backgroundImage,
+  leftBtnText,
+  rightBtnText,
+}) => {
   return (
-    <Wrap>
+    <Wrap bgImage={backgroundImage}>
       <ItemText>
-        <h1>Model S</h1>
-        <p>Order Online for Touchless Delivery</p>
+        <h1>{title}</h1>
+        <p>{description}</p>
       </ItemText>
-      <ButtonGroup>
-        <LeftButton>Custom Order</LeftButton>
-        <RightButton>Existing Inventory</RightButton>
-      </ButtonGroup>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{leftBtnText}</LeftButton>
+          {/* conditional rendering. */}
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+        </ButtonGroup>
+        <DownArrow src={downArrow}></DownArrow>
+      </Buttons>
     </Wrap>
   );
 };
@@ -26,6 +37,13 @@ const Wrap = styled.div`
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  // vertical alignment
+  justify-content: space-between;
+  // horizontal alignment
+  align-items: center;
+  background-image: ${(props) => `url("${props.bgImage}")`};
 `;
 
 const ItemText = styled.div`
@@ -34,13 +52,42 @@ const ItemText = styled.div`
 `;
 
 const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
 
-`
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
 
 const LeftButton = styled.div`
-
-`
+  background: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  // sets the transparency of an element
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
 // inherit all the css of the leftButton
 const RightButton = styled(LeftButton)`
+  background: #fff;
+  color: #000;
+  opacity: 0.65;
+`;
 
-`
+const DownArrow = styled.img`
+  margin-top: 20px;
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+  overflow-x: hidden;
+`;
+
+const Buttons = styled.div``;
